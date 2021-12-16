8e8636180c30b3887454b1789353cec505e11f0b
qetiproject <keti.xecuriani@gmail.com>
2021-12-16 20:10:31 +0400

    save arr in localstorage doesn't work

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [number, setNumber] = useState([])
+  const [error, setError] = useState('')
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const storeData = async (value) => {
+    number.map((n) => {
+      if(value === n){
+        setError('ასეთი ციფრი უკვე არსებობს')
+      } else {
+        setNumber(number => [...number, value])
+      }
+    })
+    
+    await AsyncStorage.setItem('@input', JSON.stringify(number))
+  }
+
+  const fetchData =  (value) => {
+    value > 100 ? setError('შეიყვანეთ 1-დან 100-მდე ციფრი') : setError(''); setNumber(value);
+  }
+
+  const deleteData = async () => {
+    await AsyncStorage.removeItem( '@input')
+    setNumber('')
+  }
+
+  useEffect( async () => {
+    const value = await AsyncStorage.getItem('@input')
+    if(number !==null) {
+      setNumber( value)
+    }
+  }, [])
+
+  
+  return (
+      <View style={
+        {
+          margin: 100
+        }
+      }>
+        <Text>{JSON.stringify(number)}</Text>
+        <Text>{error}</Text>
+        <TextInput onChangeText={ (n)=> fetchData(n)}/>
+        <Button 
+          title="store data"
+          onPress={() => storeData(number)}
+        />
+        <Button 
+          color = "red"
+          title="delete data"
+          onPress={() => deleteData(number)}
+        />
+      </View>
+  );
+  
+}
