5fb418e9702f95b4b1b43fbfe19fbe2e1f57f57e
qetiproject <keti.xecuriani@gmail.com>
2021-12-18 15:50:42 +0400

    styling

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Text, TextInput, View } from 'react-native';
+import {Text, View } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import styled from 'styled-components/native'
 
 export default function App() {
   const [number, setNumber] = useState([])
   const [error, setError] = useState('')
+
+  const TextInput = styled.TextInput`
+    maxWidth: 90%,
+    borderBottomColor: blue;
+    borderBottomWidth: 3
+  `
+
+  const ButtonText = styled.Button`
+    borderBottomColor: blue;
+  `
 
   const storeData = async (value) => {
     number.map((n) => {
@@ -22,10 +33,6 @@ export default function App() {
     value > 100 ? setError('შეიყვანეთ 1-დან 100-მდე ციფრი') : setError(''); setNumber(value);
   }
 
-  const deleteData = async () => {
-    await AsyncStorage.removeItem( '@input')
-    setNumber('')
-  }
 
   useEffect( async () => {
     const value = await AsyncStorage.getItem('@input')
@@ -43,15 +50,14 @@ export default function App() {
       }>
         <Text>{JSON.stringify(number)}</Text>
         <Text>{error}</Text>
-        <TextInput onChangeText={ (n)=> fetchData(n)}/>
-        <Button 
+        <TextInput  
+          color="#000" 
+          placeholder="შეიყვანეთ ციფრი 1-დან 100-მდე" 
+          onChangeText={ (n)=> fetchData(n)}
+        />
+        <ButtonText 
           title="store data"
           onPress={() => storeData(number)}
-        />
-        <Button 
-          color = "red"
-          title="delete data"
-          onPress={() => deleteData(number)}
         />
       </View>
   );
